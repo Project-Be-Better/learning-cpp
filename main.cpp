0e27f0ddb2e261482da244ff0fc90b97ae076902
sree <sreeraj_ec@hotmail.com>
2025-03-13 21:28:51 +0800

    Update: First Function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
+    std::cout << "Hello, World!" << std::endl;
+
+    int firstNumber = 2;
+    int secondNumber = 3;
+    int sum = firstNumber + secondNumber;
+    std::cout << "The sum of first number and second number is: " << sum << std::endl;
     return 0;
 }
