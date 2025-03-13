8961afe5189f185c171feb845a012836bc4cd686
sree <sreeraj_ec@hotmail.com>
2025-03-13 19:13:21 +0800

    Update: Comments

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+    // This is a comment
+
+    /**
+     * This is a multi-line comment
+     */
+
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Number 1" << std::endl;
     std::cout << "Number 2" << std::endl;
