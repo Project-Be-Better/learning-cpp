11515069a6d20a23551f8384f9aac4b6253bd2cf
sree <sreeraj_ec@hotmail.com>
2025-03-31 12:09:50 +0800

    D002-bashscript-updated

--- D002-include-guards/correct-version/command.bash
+++ D002-include-guards/correct-version/command.bash
@@ -1,7 +1,18 @@
 #!/bin/bash
-rm -rf build
-cd build || mkdir build && cd build
+
+BUILD_DIR=build
+EXECUTABLE=main
+
+if [ "$1" == "clean" ]; then 
+    echo "Cleaning up..."
+    rm -rf "$BUILD_DIR"
+fi 
+
+mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR" || exit 1
+
+echo "Building..."
+
 cmake ..
 cmake --build .
 echo "Running..."
-./main
+./$EXECUTABLE
