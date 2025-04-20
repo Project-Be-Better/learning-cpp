f2701ac5914a78ecaf31f169500fdc0098a486c7
sree <sreeraj_ec@hotmail.com>
2025-04-20 08:55:40 +0800

    D011

---
+++ learncpp-hello-world/learncpp-hello-world.cpp
@@ -0,0 +1,12 @@
+﻿// learncpp-hello-world.cpp : Defines the entry point for the application.
+//
+
+#include "learncpp-hello-world.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
