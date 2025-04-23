288edf190b7f875f34f4c3b4cf82edc60ef5fcd8
sree <sreeraj_ec@hotmail.com>
2025-04-23 09:29:03 +0800

    D014-20250423-Conan setup completed

--- project-teletrack-sim/build-wsl.sh
+++ project-teletrack-sim/build-wsl.sh
@@ -5,15 +5,55 @@ set -e
 
 # VARIABLES
 # ---------
-
 BUILD_DIR="build"                           # Directory where build artifacts will be placed 
 EXE_PATH="app/Debug/project-teletrack-sim"  # Relative path to build once it is built 
+VENV_PATH=".venv-conan"
+ACTIVATE_VENV_SCRIPT="$VENV_PATH/Scripts/activate"
 
+# HANDLE VENV
+# -----------
+
+if [ ! -d "$VENV_PATH" ];then                 # If no venv exists, create a new one
+    echo "🤖 Creating Python venv for Conan at $VENV_PATH"
+    python -m venv "$VENV_PATH"
+else
+    echo "🟢 venv already exists at $VENV_PATH"
+fi 
+
+echo "🤖 Activating venv"
+source "$ACTIVATE_VENV_SCRIPT"
+
+# HANDLE CONAN
+# ----------------
+if command -v conan > /dev/null; then
+    echo "🟢 Conan exists at $VENV_PATH"
+else 
+    echo "🤖 Installing Conan inside venv..."
+    pip install conan    
+fi
 
 # CREATE DIRECTORY
 # ----------------
-mkdir -p "$BUILD_DIR"                       # Create build directory and enter it 
+if [ ! -d "$BUILD_DIR" ];then 
+    echo "🤖 Creating build folder..."
+    mkdir -p "$BUILD_DIR"                   # Create build directory and enter it 
+fi 
+
 cd "$BUILD_DIR"                             # Change to the build directory
+
+# RUN CONAN and HANDLE CONAN PROFILE
+# ----------------------------------
+CONAN_PROFILE_PATH="$HOME/.conan2/profiles/default"
+if [ ! -f "$CONAN_PROFILE_PATH" ]; then
+    echo "🤖 Conan profile not found — generating default profile..."
+    conan profile detect --force
+else
+    echo "🟢 Conan default profile already exists"
+fi
+
+
+echo "🤖 Running Conan..."
+conan install .. --output-folder=. --build=missing
 
 # RUN CMAKE TO CONFIGURE THE PROJECT 
 # ----------------------------------
