234df32ec9315330eae045d700b93e1e683bf9b3
sree <sreeraj_ec@hotmail.com>
2025-04-29 08:45:23 +0800

    D019-20250429: Observer pattern token

---
+++ D019-observer/README.md
@@ -0,0 +1,1 @@
+![](./img/concept.png)
--- D019-observer/src/main.cpp
+++ D019-observer/src/main.cpp
@@ -2,8 +2,41 @@
 #include <list>
 #include <string>
 
+// Subscriber Interface
+class IObserver
+{
+public:
+    virtual ~IObserver() {};
+    virtual void Update(const std::string &message_from_subject) = 0;
+};
+
+// Publisher Interface
+class IObject
+{
+};
+
+class Observer : public IObserver
+{
+public:
+    Observer() {};
+
+    virtual ~Observer()
+    {
+        std::cout << "Observer " << this->number_ << "is deleted \n";
+    }
+
+private:
+    int number_;
+};
+
+void clientCode()
+{
+    std::cout << "Running Client Code \n";
+};
+
 int main()
 {
     std::cout << "Running Observer tests.. \n";
+    clientCode();
     return 0;
 }
